840af4928b89d7cde43c585fab7a4fa731e2133e
Alex Clark <amsclark@gmail.com>
2022-12-22 07:55:47 -0600

    gui work

--- ncriat.py
+++ ncriat.py
@@ -4,6 +4,7 @@
 
 import tkinter as tk
 from tkinter import *
+from functools import partial
 import sys
 import datetime
 import urllib
@@ -13,56 +14,39 @@ from bs4 import BeautifulSoup
 import csv
 
 def getCaseList():
-  pass
+  print("getting case list")
 
 def updateCaseList():
-  pass
+  print("updating case list")
 
 def getCases():
-  pass
+  print("getting cases")
 
 def getCase():
-  pass
+  print("getting case ")
 
 def extractInfo():
-  pass
+  print("extracting info from")
 
 def createSpreadsheet():
-  pass
+  print("creating spreadsheet")
 
-def constructMainWindow(root):
-  root.title("Nebraska Courts E-Services Scraper") 
-  # date options area
-  date_frame = LabelFrame(root, text="Target Date", padx=5, pady=5, relief=RIDGE)
-  label1 = Label(date_frame, text="Please enter a date \n in mm/dd/yyyy format.")
-  entry1=Entry(date_frame)
+def constructFilterWindow(root, button1, button2, cred_frame, user_entry_label, user_entry, pass_entry_label, pass_entry):
+  root.title("Nebraska Criminal Record Information Automation Tool | Filter")
+  cred_frame.grid_remove()
+  button1.grid_remove()
+  user_entry_label.grid_remove()
+  user_entry.grid_remove()
+  pass_entry_label.grid_remove()
+  pass_entry.grid_remove()
+  button2.grid(row=0, column=1, sticky="W", padx=10, pady=10)
 
-  # county options area
-  options_frame = LabelFrame(root, text="Choose a County Option", padx=5, pady=5, relief=RIDGE)
-  c_option = tk.StringVar(None, "1")
-  option1 = Radiobutton(options_frame, text="Douglas, Lancaster and Sarpy Only", variable=c_option, value="1")
-  option2 = Radiobutton(options_frame, text="All Nebraska Counties", variable=c_option, value="2")  
+  
 
-  #button to press
-  button1 = tk.Button(text="Scrape Justice", command=scrapeCalendar)
-
-  # credentials
-  cred_frame = LabelFrame(root, text="Justice Login Credentials", padx=5, pady=5, relief=RIDGE)
-  user_entry_label = Label(cred_frame, text="Username")
-  user_entry=Entry(cred_frame)
-  pass_entry_label = Label(cred_frame, text="Password")
-  pass_entry=Entry(cred_frame)
-  pass_entry.config(show="*")
-
-  date_frame.grid(row=0, column=1, rowspan=5, padx=10, pady=10)
-  label1.grid(row=0, column=0)
-  entry1.grid(row=1, column=0)
-
-  options_frame.grid(row=0, column=2, rowspan=5, padx=10, pady=10)
-  option1.grid(row=0, column=0, sticky="W")
-  option2.grid(row=1, column=0, sticky="W")
-
-  cred_frame.grid(row=0, column=3, rowspan=5, padx=10, pady=10)
+def constructMainWindow(root, button1, cred_frame, user_entry_label, user_entry, pass_entry_label, pass_entry):
+  root.title("Nebraska Criminal Record Information Automation Tool | Login") 
+  
+  cred_frame.grid(row=0, column=1, rowspan=5, padx=10, pady=10)
   user_entry_label.grid(row=0, column=0, sticky="W")
   user_entry.grid(row=0, column=1, sticky="W")
   pass_entry_label.grid(row=1, column=0, sticky="W")
@@ -70,5 +54,22 @@ def constructMainWindow(root):
 
   button1.grid(row=7, column=1, sticky="W", padx=10, pady=10)
 
+
 root = tk.Tk()
+
+#button to press
+button2 = tk.Button(text="Retrieve Cases", command=getCases)
+button1 = tk.Button(text="Get Records", command=lambda: constructFilterWindow(root, button1, button2, cred_frame, user_entry_label, user_entry, pass_entry_label, pass_entry))
+
+# credentials
+cred_frame = LabelFrame(root, text="Justice Login Credentials", padx=5, pady=5, relief=RIDGE)
+user_entry_label = Label(cred_frame, text="Username")
+user_entry=Entry(cred_frame)
+pass_entry_label = Label(cred_frame, text="Password")
+pass_entry=Entry(cred_frame)
+pass_entry.config(show="*")
+
+
+constructMainWindow(root, button1, cred_frame, user_entry_label, user_entry, pass_entry_label, pass_entry)
+
 root.mainloop()
