e759903c2c53610507754d0625c75128718100c9
Alex Clark <amsclark@gmail.com>
2022-12-24 00:00:32 -0600

    processing results

--- ncriat.py
+++ ncriat.py
@@ -2,8 +2,6 @@
 # Creative Commons CC0 v1.0 Universal Public Domain Dedication. No Rights Reserved
 # Version 0.2
 
-from uifuncs import *
-from casefuncs import *
 
 import tkinter as tk
 from tkinter import *
@@ -19,9 +17,221 @@ import lxml
 import csv
 from openpyxl import load_workbook
 
+def constructFilterWindow(root, search_button, retrieve_button, select_all_button, clear_all_button, cred_frame, user_entry_label, user_entry, pass_entry_label, pass_entry, casetree, caselistbox_frame, def_frame, first_entry_label, first_name, last_entry_label, last_name):
+  root.title("Nebraska Criminal Record Information Automation Tool | Filter")
+  # remove all the elements from the previous view
+  cred_frame.grid_remove()
+  search_button.grid_remove()
+  user_entry_label.grid_remove()
+  user_entry.grid_remove()
+  pass_entry_label.grid_remove()
+  pass_entry.grid_remove()
+  def_frame.grid_remove()
+  first_entry_label.grid_remove()
+  first_name.grid_remove()
+  last_entry_label.grid_remove()
+  last_name.grid_remove()
+  #add new elements for this view
+  caselistbox_frame.grid(row=0, column=1, rowspan=5, columnspan=30, padx=10, pady=10)
+  casetree.grid(row=0, column=0, sticky="W")
+  caseList = getCaseList(first_name, last_name, user_entry, pass_entry)
+  for case in caseList:
+      casetree.insert('', END, values=case)
+  retrieve_button.grid(row=7, column=1, sticky="W", padx=10, pady=10)
+  select_all_button.grid(row=7, column=2, sticky="W", padx=10, pady=10)
+  clear_all_button.grid(row=7, column=3, sticky="W", padx=10, pady=10)
 
+  
 
+def constructMainWindow(root, search_button, cred_frame, user_entry_label, user_entry, pass_entry_label, pass_entry, def_frame, first_entry_label, first_name, last_entry_label, last_name):
+  root.title("Nebraska Criminal Record Information Automation Tool | Login") 
+  cred_frame.grid(row=0, column=1, rowspan=5, padx=10, pady=10)
+  user_entry_label.grid(row=0, column=0, sticky="W")
+  user_entry.grid(row=0, column=1, sticky="W")
+  pass_entry_label.grid(row=1, column=0, sticky="W")
+  pass_entry.grid(row=1, column=1, sticky="W")
 
+  def_frame.grid(row=0, column=2, rowspan=5, padx=10, pady=10)
+  first_entry_label.grid(row=0, column=0, sticky="W")
+  first_name.grid(row=0, column=1, sticky="W")
+  last_entry_label.grid(row=1, column=0, sticky="W")
+  last_name.grid(row=1, column=1, sticky="W")
+
+  search_button.grid(row=7, column=1, sticky="W", padx=10, pady=10)
+
+def getCountyName(lookup_number):
+  county_numbers_dict = {"Adams" : "14",
+  "Antelope" : "26",
+  "Arthur" : "91",
+  "Banner" : "85",
+  "Blaine" : "86",
+  "Boone" : "23",
+  "Box Butte" : "65",
+  "Boyd" : "63",
+  "Brown" : "75",
+  "Buffalo" : "09",
+  "Burt" : "31",
+  "Butler" : "25",
+  "Cass" : "20",
+  "Cedar" : "13",
+  "Chase" : "72",
+  "Cherry" : "66",
+  "Cheyenne" : "39",
+  "Clay" : "30",
+  "Colfax" : "43",
+  "Cuming" : "24",
+  "Custer" : "04",
+  "Dakota" : "70",
+  "Dawes" : "69",
+  "Dawson" : "18",
+  "Deuel" : "78",
+  "Dixon" : "35",
+  "Dodge" : "05",
+  "Douglas" : "01",
+  "Dundy" : "76",
+  "Fillmore" : "34",
+  "Franklin" : "50",
+  "Frontier" : "60",
+  "Furnas" : "38",
+  "Gage" : "03",
+  "Garden" : "77",
+  "Garfield" : "83",
+  "Gosper" : "73",
+  "Grant" : "92",
+  "Greeley" : "62",
+  "Hall" : "08",
+  "Hamilton" : "28",
+  "Harlan" : "51",
+  "Hayes" : "79",
+  "Hitchcock" : "67",
+  "Holt" : "36",
+  "Hooker" : "93",
+  "Howard" : "49",
+  "Jefferson" : "33",
+  "Johnson" : "57",
+  "Kearney" : "52",
+  "Keith" : "68",
+  "Keya Paha" : "82",
+  "Kimball" : "71",
+  "Knox" : "12",
+  "Lancaster" : "02",
+  "Lincoln" : "15",
+  "Logan" : "87",
+  "Loup" : "88",
+  "Madison" : "07",
+  "McPherson" : "90",
+  "Merrick" : "46",
+  "Morrill" : "64",
+  "Nance" : "58",
+  "Nemaha" : "44",
+  "Nuckolls" : "42",
+  "Otoe" : "11",
+  "Pawnee" : "54",
+  "Perkins" : "74",
+  "Phelps" : "37",
+  "Pierce" : "40",
+  "Platte" : "10",
+  "Polk" : "41",
+  "Red Willow" : "48",
+  "Richardson" : "19",
+  "Rock" : "81",
+  "Saline" : "22",
+  "Sarpy" : "59",
+  "Saunders" : "06",
+  "Scotts Bluff" : "21",
+  "Seward" : "16",
+  "Sheridan" : "61",
+  "Sherman" : "56",
+  "Sioux" : "80",
+  "Stanton" : "53",
+  "Thayer" : "32",
+  "Thomas" : "89",
+  "Thurston" : "55",
+  "Valley" : "47",
+  "Washington" : "29",
+  "Wayne" : "27",
+  "Webster" : "45",
+  "Wheeler" : "84",
+  "York" : "17"}
+  for key, value in county_numbers_dict.items():
+    if lookup_number == value:
+      return key  
+  return "no such county"
+
+def selectAll(casetree):
+    for item in casetree.get_children():
+        casetree.selection_add(item)
+
+def clearAll(casetree):
+    for item in casetree.get_children():
+        casetree.selection_remove(item)
+        
+def getCaseList(first_name, last_name, user_entry, pass_entry):
+  caseListData = []
+  caseList = []
+  first_name_string = first_name.get()
+  last_name_string = last_name.get()
+  username = user_entry.get()
+  password = pass_entry.get()
+  print("getting case list for " + first_name_string + " " + last_name_string + " with username " + username + " and password " + password)
+  JUSTICE_URL = 'https://www.nebraska.gov/justice/name.cgi'
+  data = {
+      'party_name': last_name_string + ', ' + first_name_string,
+      'indiv_entity_type': 'individual',
+      'case_type': [
+          'CR',
+          'TR',
+       ],
+      'submit_hidden': '1',
+      'sort': 'party_name',
+      'order': 'asc',
+      'year': '',
+      }
+  resp = requests.post(JUSTICE_URL, timeout=180, auth=HTTPBasicAuth(username, password), data=data)
+  soup = BeautifulSoup(resp.content, 'lxml')
+  rows = soup.find_all('tr')
+  for row in rows:
+      cols = row.find_all('td')
+      cols = [ele.text.strip() for ele in cols]
+      caseListData.append([ele for ele in cols if ele]) # get rid of empty values
+  
+  for caseListDatum in caseListData:
+    if (len(caseListDatum) > 0):
+      name = caseListDatum[0].split(" (\xa0")[0]
+      party_type = caseListDatum[0].split("\xa0)\nDOB: ")[0].split(" (\xa0")[1]
+      if "DOB: "in caseListDatum[0]:
+        DOB = caseListDatum[0].split("DOB: ")[1]
+      else:
+        DOB = ""
+      county = getCountyName("".join(caseListDatum[1].split())[1:3])
+      case_num = "".join(caseListDatum[1].split())
+      caption = caseListDatum[2]
+      judge = caseListDatum[3]
+      attorney = caseListDatum[4]
+      caseListItem = (name, party_type, DOB, county, case_num, caption, judge, attorney)
+      caseList.append(caseListItem)
+      
+
+  return caseList
+
+def updateCaseList():
+  print("updating case list")
+
+def getCases(casetree):
+  print("getting cases")
+  selected_cases = casetree.selection()
+  print(selected_cases)
+
+  
+
+def getCase():
+  print("getting case ")
+
+def extractInfo():
+  print("extracting info from")
+
+def createSpreadsheet():
+  print("creating spreadsheet")
 
 
 
